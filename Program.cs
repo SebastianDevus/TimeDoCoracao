05084965eccdf33cb08e58965acc9a0fecd921d8
LAB-00\lab00-00 <sebastiandevus@gmail.com>
2022-08-09 22:29:23 -0300

    Exercício realizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿MudaCor();
+Console.Write("\nCo");
+
+Console.ResetColor();
+Console.Write("ri");
+
+MudaCor();
+Console.Write("nt");
+
+Console.ResetColor();
+Console.Write("hi");
+
+MudaCor();
+Console.Write("an");
+
+Console.ResetColor();
+Console.WriteLine("s.\n");
+
+void MudaCor()
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.White;
+}
